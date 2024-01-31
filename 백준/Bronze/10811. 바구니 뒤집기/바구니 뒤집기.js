d310bc676eea23bef0b7b24da6a58be2693b98ff
eunseopi <117704586+eunseopi@users.noreply.github.com>
2024-01-31 17:02:18 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 140 ms, Memory: 9444 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
@@ -0,0 +1,18 @@
+const input = require("fs").readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M] = input[0].trim().split(" ").map(x => parseInt(x));
+const arr = [];
+for(let i = 0; i < N; i++){
+    arr.push(i+1);
+}
+
+for(let j = 0; j < M; j++){
+    let[before,after] = input[j+1].split(" ").map(x => parseInt(x));
+    let basket = [];
+    for(let j = before - 1; j<after; j++){
+        basket.push(arr[j]);
+    };
+    basket.reverse();
+    arr.splice(before-1, after-before+1, ...basket)
+}
+console.log(arr.join(" "));
